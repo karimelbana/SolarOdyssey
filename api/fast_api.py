8759ab81c48f356e921d213d8c38a17f83564355
Jonathan Buening <JoChrisMaBue@gmail.com>
2023-03-15 11:37:15 +0100

    Changed fast_api predict method to take in an image in the form of files.

--- api/fast_api.py
+++ api/fast_api.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
 from PIL import Image
-from fastapi import UploadFile, File
 from tensorflow.keras.applications.densenet import preprocess_input as preprocess_input_densenet
 from tensorflow.keras.models import load_model
 from io import BytesIO
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -25,11 +24,10 @@ model_path = "Interface/the_best_model.h5"
 app.state.model = load_model(model_path)
 
 
-
+# Takes in a file and returns a prediction
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
     contents = await file.read()
-
 
     # Read the bytes back into an image format, then convert to a numpy array
     image = np.array(Image.open(BytesIO(contents)))
